c811b08551e639ab0c2cb2554a276b8b9357cbb8
maorunjie <2541340695@qq.com>
2023-04-18 13:42:35 +0800

    feat: deploy app1

--- js/about.f2afcc68.js
+++ js/about.f2afcc68.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunksub_app1"]=self["webpackChunksub_app1"]||[]).push([[443],{1327:function(a,t,u){u.r(t),u.d(t,{default:function(){return r}});var s=function(){var a=this,t=a._self._c;return t("div",{staticClass:"about"},[t("h1",[a._v("This is an about page app1"+a._s(a.a))])])},e=[],n={data(){return{a:""}},mounted(){this.a=window.a,console.log("app1 about mounted")},destroyed(){console.log("app1 about destoryed")}},o=n,l=u(1128),p=(0,l.Z)(o,s,e,!1,null,null,null),r=p.exports}}]);
-//# sourceMappingURL=about.a990b3a5.js.map
+//# sourceMappingURL=about.f2afcc68.js.map
